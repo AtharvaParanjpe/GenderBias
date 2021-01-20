b3c0683a98f13dcacbb41c69d31da08a8277f620
AtharvaParanjpe <atharvapparanjpe@gmail.com>
2021-01-20 22:52:44 +0530

    Updated Files

---
+++ buildDataset.py
@@ -0,0 +1,247 @@
+import numpy as np
+import csv
+import pandas as pd
+
+def readFile(fileName):
+    file = pd.read_csv(fileName)
+    return file
+
+def calcPrevOccurences(patientIds, dateOfAtttempt, file, date, columnName):
+    prevVisits = []
+    for x, y in zip(patientIds, dateOfAtttempt):
+        prev = []
+        attempt = y.split('-')
+        if(len(attempt)==3):
+            for w,z in zip(file.get_group(x)[columnName].values,file.get_group(x)[date].values):
+                prevDate = z.split('-')
+                if(len(prevDate)==3):
+                    prev.append([w,z])
+        prevVisits.append( prev)
+    return prevVisits
+
+def filterBySuicideTendancy(file,columnName, date):
+    print(columnName)
+    currentFile = file.groupby(['PATIENT'])
+    file[columnName] = file[columnName].str.lower()
+    file.dropna(subset = [columnName])
+    file = file[file[columnName].str.contains('sui', na=False ) ]
+    
+    patientIds = list(file["PATIENT"].values)
+    dateOfAtttempt = list(file[date].values)
+
+    prevVisits = calcPrevOccurences(patientIds, dateOfAtttempt, currentFile, date, columnName)
+
+    return patientIds, dateOfAtttempt, prevVisits
+
+def pickRandomData(file,columnName,date):
+    currentFile = file.groupby(['PATIENT'])
+    file = file[~file[columnName].str.contains('sui', na=False )]
+    patientIds = list(file["PATIENT"].values)
+    dateOfVisit = list(file[date].values)
+    prevVisits = calcPrevOccurences(patientIds, dateOfVisit, currentFile, date, columnName)
+    return patientIds, dateOfVisit, prevVisits
+
+
+
+## not needed
+
+## needed
+
+abcd = 1
+
+csvFileName = 'csv'+str(abcd)
+
+allergiesFile = readFile('./CSV/' + csvFileName +'/allergies.csv')
+immunizationsFile = readFile('./CSV/' + csvFileName +'/immunizations.csv')
+medicationsFile = readFile('./CSV/' + csvFileName +'/medications.csv')
+observationsFile = readFile('./CSV/' + csvFileName +'/observations.csv')
+
+carePlansFile= readFile('./CSV/' + csvFileName +'/careplans.csv')
+conditionsFile = readFile('./CSV/' + csvFileName +'/conditions.csv')
+encountersFile = readFile('./CSV/' + csvFileName +'/encounters.csv')
+proceduresFile = readFile('./CSV/' + csvFileName +'/procedures.csv')
+
+
+
+patientsColumnNames = ["ID","BIRTHDATE","DEATHDATE","SSN","DRIVERS","PASSPORT","PREFIX","FIRST","LAST","SUFFIX","MAIDEN","MARITAL","RACE","ETHNICITY","GENDER","BIRTHPLACE","ADDRESS",'SUICIDE_AGE','SUICIDE_YEAR','PREV_ENCOUNTERS']
+
+patientIdListForSuicide = []
+datesList = []
+prevOccurences = []
+
+def appendToExistingData(file,columnName,date):
+    global patientIdListForSuicide
+    global datesList
+    global prevOccurences
+
+    ids, dates, prevVisits = filterBySuicideTendancy(file, columnName,date)
+    patientIdListForSuicide+= ids
+    datesList += dates
+    prevOccurences+=prevVisits
+
+def generate_positive_samples():
+    global prevOccurences
+
+    appendToExistingData(carePlansFile, "DESCRIPTION",'START')
+    appendToExistingData(carePlansFile, "REASONDESCRIPTION", 'START')
+    print("Done with 1")
+
+    appendToExistingData(conditionsFile, "DESCRIPTION", 'START')
+    print("Done with 2")
+
+    appendToExistingData(encountersFile, "DESCRIPTION", 'DATE')
+    appendToExistingData(encountersFile, "REASONDESCRIPTION", 'DATE')
+    print("Done with 3")
+
+    appendToExistingData(proceduresFile, "DESCRIPTION",'DATE')
+    appendToExistingData(proceduresFile, "REASONDESCRIPTION",'DATE')
+    print("Done with 4")
+
+    appendToExistingData(immunizationsFile, "DESCRIPTION",'DATE')
+    print("Done with 5")
+
+    appendToExistingData(observationsFile, "DESCRIPTION",'DATE')
+    print("Done with 6")
+
+    appendToExistingData(medicationsFile, "DESCRIPTION",'START')
+    appendToExistingData(medicationsFile, "REASONDESCRIPTION",'START')
+    print("Done with 7")
+
+    appendToExistingData(allergiesFile, "DESCRIPTION",'START')
+    print("Done with 8")
+
+    a = np.unique(patientIdListForSuicide, return_index=True)
+
+    finalPatientIds = np.take(patientIdListForSuicide, a[1])
+    finalDateList = np.take(datesList, a[1])
+    prevOccurences = np.take(prevOccurences,a[1])
+
+    dictionary = {}
+    prevOccurencesDictionary = {}
+    for x,y,z in zip(finalDateList, finalPatientIds, prevOccurences):
+        dictionary[y] = x
+        prevOccurencesDictionary[y] = z
+
+    patientRecords = []
+    with open('./CSV/' + csvFileName +'/patients.csv') as csvfile:
+        readCSV = csv.reader(csvfile, delimiter=',')
+        count = 0
+        for row in readCSV:
+            if(row and len(row)==17 and row[0]!="" and row[0] in finalPatientIds):
+                birthYear = int(row[1].split('-')[0])##,int(row[1].split('-')[1]),int(row[1].split('-')[2])
+                suicideYear = int(dictionary[row[0]].split('-')[0])
+                row.append(str(suicideYear-birthYear))
+                row.append(dictionary[row[0]].split('-'))
+                
+                prevOccurences = prevOccurencesDictionary[row[0]]
+                newListOfOccurences = []
+                for x in prevOccurences:
+                    reason, time = x
+                    # print(reason,time)
+                    # input()
+                    visitYear = int(time.split('-')[0])
+                    # visitYear, visitMonth, visitDay = int(time.split('-')[0]),int(time.split('-')[1]),int(time.split('-')[2])
+                    # if(visitYear<birthYear and visitMonth < birthMonth and visitDay < birthDate):
+                    #     continue
+                    newListOfOccurences.append([reason, str(visitYear-birthYear)])
+                row.append(newListOfOccurences)
+                patientRecords.append(row)
+
+    print(len(patientRecords))
+
+    patientRecordsDf = pd.DataFrame(data=patientRecords, columns=patientsColumnNames)
+    print(patientRecordsDf.head(100))
+
+    patientRecordsDf.to_csv("./Modified Outputs/Modified Output "+str(abcd))
+
+patientIdListForOthers = []
+datesListForOthers = []
+prevOccurences = []
+
+# generate_positive_samples()
+
+def appendNegativeSamples(file, columnName, date):
+    global patientIdListForOthers
+    global datesListForOthers
+    global prevOccurences
+
+    ids, dates, prevVisits = pickRandomData(file, columnName,date)
+    patientIdListForOthers+= ids
+    datesListForOthers += dates
+    prevOccurences+=prevVisits
+
+
+def generate_negative_samples():
+    global prevOccurences
+
+    appendNegativeSamples(carePlansFile,  "DESCRIPTION",'START')
+    appendNegativeSamples(carePlansFile, "REASONDESCRIPTION", 'START')
+    print("Done with 1")
+    
+    appendNegativeSamples(conditionsFile, "DESCRIPTION", 'START')
+    print("Done with 2")
+
+    appendNegativeSamples(encountersFile, "DESCRIPTION", 'DATE')
+    appendNegativeSamples(encountersFile, "REASONDESCRIPTION", 'DATE')
+    print("Done with 3")
+
+
+    appendNegativeSamples(proceduresFile, "DESCRIPTION",'DATE')
+    appendNegativeSamples(proceduresFile, "REASONDESCRIPTION",'DATE')
+    print("Done with 4")
+
+    appendNegativeSamples(immunizationsFile, "DESCRIPTION",'DATE')
+    print("Done with 5")
+
+    appendNegativeSamples(observationsFile, "DESCRIPTION",'DATE')
+    print("Done with 6")
+
+    appendNegativeSamples(medicationsFile, "DESCRIPTION",'START')
+    appendNegativeSamples(medicationsFile, "REASONDESCRIPTION",'START')
+    print("Done with 7")
+
+    appendToExistingData(allergiesFile, "DESCRIPTION",'START')
+    print("Done with 8")
+
+    a = np.unique(patientIdListForOthers, return_index=True)
+
+    finalPatientIds = np.take(patientIdListForOthers, a[1])
+    finalDateList = np.take(datesListForOthers, a[1])
+    prevOccurences = np.take(prevOccurences,a[1])
+
+    dictionary = {}
+
+    prevOccurencesDictionary = {}
+    for x,y,z in zip(finalDateList, finalPatientIds, prevOccurences):
+        dictionary[y] = x
+        prevOccurencesDictionary[y] = z
+
+    patientRecords = []
+    with open('./CSV/' + csvFileName +'/patients.csv') as csvfile:
+        readCSV = csv.reader(csvfile, delimiter=',')
+        count = 0
+        for row in readCSV:
+            if(row and len(row)==17 and row[0]!="" and row[0] in finalPatientIds):
+                birthYear = int(row[1].split('-')[0])
+                suicideYear = int(dictionary[row[0]].split('-')[0])
+                row.append(str(suicideYear-birthYear))
+                row.append(dictionary[row[0]].split('-'))
+                
+                prevOccurences = prevOccurencesDictionary[row[0]]
+                newListOfOccurences = []
+                for x in prevOccurences:
+                    reason, time = x
+                    visitYear = int(time.split('-')[0])
+                    
+                    newListOfOccurences.append([reason, str(visitYear-birthYear)])
+                row.append(newListOfOccurences)
+                patientRecords.append(row)
+                
+    print(len(patientRecords))
+
+    patientRecordsDf = pd.DataFrame(data=patientRecords, columns=patientsColumnNames)
+    print(patientRecordsDf.head(100))
+
+    patientRecordsDf.to_csv("./Negative Samples/neg "+str(abcd))
+
+generate_negative_samples()  
--- computeBias.py
+++ computeBias.py
@@ -52,12 +52,12 @@ def getData():
     df_positive = df_positive.assign(target=1)
     df_negative = df_negative.assign(target=0)
     df = df_positive.append(df_negative, ignore_index=True)
+    df = shuffle(df)
     df = cleanData(df)
     return df
 df = getData()
-print(df.head(10))
+# print(df.head(5))
 
-# input()
 
 from sklearn.preprocessing import LabelEncoder
 le = LabelEncoder()
@@ -70,6 +70,9 @@ def preProcessData(df):
     df[categorical_cols] = df[categorical_cols].apply(lambda col: le.fit_transform(col))
     return df
 df_labelEncoded = preProcessData(df)
+# print(df_labelEncoded.head(5))
+
+
 ############################ SMOTE TO BALANCE THE DATA ###########################
 
 from imblearn.over_sampling import SMOTE
@@ -86,9 +89,8 @@ x['GENDER'] = y
 df = shuffle(x)
 
 df = df.groupby(['GENDER'])
+
 ##################################################################################
-
-
 
 
 from sklearn.model_selection import train_test_split
@@ -97,7 +99,7 @@ from sklearn import metrics
 from sklearn.metrics import classification_report, confusion_matrix
 
 
-def compute_metrics(TP, TN, FP, FN):
+def compute_metrics(TP, TN, FP, FN, auc):
     TPR = TP/(TP+FN)
     TNR = TN/(TN+FP) 
     # PPV = TP/(TP+FP)
@@ -106,84 +108,113 @@ def compute_metrics(TP, TN, FP, FN):
     FNR = FN/(TP+FN)
     # FDR = FP/(TP+FP)
     ACC = (TP+TN)/(TP+FP+FN+TN)
-    return [TPR, TNR, FPR, FNR, ACC]
+    return [TPR, TNR, FPR, FNR, ACC, auc]
 
 
 def compute_svm(data):
-    global x
-    global y
+    
     y = data['target']
     x = data
     x = x.drop('target', axis = 1)
-    X_train, X_test, y_train, y_test = train_test_split(x, y, test_size = 0.30)
+    X_train, X_test, y_train, y_test = train_test_split(x, y, test_size = 0.15)
     svclassifier = SVC(kernel='linear')
     svclassifier.fit(X_train, y_train)
     y_pred = svclassifier.predict(X_test)
     # print(confusion_matrix(y_test,y_pred))
-    # fpr, tpr, thresholds = metrics.roc_curve(y_test, y_pred)
-    # print(fpr)
-    # print(tpr)
-    # print(metrics.auc(fpr, tpr))
-    # print("-----------------------x--------------------x---------")
     tn, fp, fn, tp = confusion_matrix(y_test, y_pred).ravel()
-    return compute_metrics(tp, tn, fp, fn)
+    fpr, tpr, thresholds = metrics.roc_curve(y_test, y_pred)
+    auc = metrics.auc(fpr, tpr)
+    return compute_metrics(tp, tn, fp, fn, auc)
 
 ################ Linear Model ################
 
-from sklearn import linear_model
+from sklearn import linear_model, tree
 
 def linearRegression(data):
-    global x
-    global y
+    
     y = data['target']
     x = data
     x = x.drop('target', axis = 1)
     reg = linear_model.LinearRegression()
-    X_train, X_test, y_train, y_test = train_test_split(x, y, test_size = 0.30, random_state=0)
+    X_train, X_test, y_train, y_test = train_test_split(x, y, test_size = 0.15, random_state=0)
     reg.fit(X_train,y_train)
     y_pred = reg.predict(X_test)
     y_pred = np.array(y_pred)
     y_pred = np.where(y_pred<0,0,1)
     # print(confusion_matrix(y_test,y_pred))
-    # fpr, tpr, thresholds = metrics.roc_curve(y_test, y_pred)
-    # print(tpr)
-    # print(fpr)
-    # print(metrics.auc(fpr, tpr))
-    # print("-----------------------x--------------------x---------")
-    # return [tpr[1], fpr[1], metrics.auc(fpr, tpr)]
     tn, fp, fn, tp = confusion_matrix(y_test, y_pred).ravel()
-    return compute_metrics(tp, tn, fp, fn)
+    fpr, tpr, thresholds = metrics.roc_curve(y_test, y_pred)
+    auc = metrics.auc(fpr, tpr)
+    return compute_metrics(tp, tn, fp, fn, auc)
 
 
 def logisticRegression(data):
-    global x
-    global y
+    
     y = data['target']
     x = data
     x = x.drop('target', axis = 1)
-    X_train, X_test, y_train, y_test = train_test_split(x, y, test_size = 0.20, random_state=0)
-    LR = linear_model.LogisticRegression(max_iter=500)
+    X_train, X_test, y_train, y_test = train_test_split(x, y, test_size = 0.15, random_state=0)
+    LR = linear_model.LogisticRegression(max_iter=1000)
     LR.fit(X_train, y_train)
     y_pred = LR.predict(X_test)
-    # fpr, tpr, thresholds = metrics.roc_curve(y_test, y_pred)
-    # print(tpr)
-    # print(fpr)
-    # print(metrics.auc(fpr, tpr))
-    # print("-----------------------x--------------------x------------------")
-    # return [tpr[1], fpr[1], metrics.auc(fpr, tpr)]
+    fpr, tpr, thresholds = metrics.roc_curve(y_test, y_pred)
     tn, fp, fn, tp = confusion_matrix(y_test, y_pred).ravel()
-    return compute_metrics(tp, tn, fp, fn)
+    auc = metrics.auc(fpr, tpr)
+    return compute_metrics(tp, tn, fp, fn, auc)
+
+def decision_tree(data):
+    y = data['target']
+    x = data
+    x = x.drop('target', axis = 1)
+    X_train, X_test, y_train, y_test = train_test_split(x, y, test_size = 0.15, random_state=0)
+    clf = tree.DecisionTreeClassifier()
+    clf = clf.fit(X_train, y_train)
+    y_pred = clf.predict(X_test)
+    fpr, tpr, thresholds = metrics.roc_curve(y_test, y_pred)
+    tn, fp, fn, tp = confusion_matrix(y_test, y_pred).ravel()
+    auc = metrics.auc(fpr, tpr)
+    return compute_metrics(tp, tn, fp, fn, auc)
+    
+
+# print("TPR, TNR, FPR, FNR, Accuracy, AUC")
+final_list = []
 
 for key,group in df:
+    group = group.drop(["GENDER"], axis=1)
     a = []
     b = []
     c = []
+    d = []
+    group = shuffle(group)
     for i in range(100):
-        a.append(linearRegression(shuffle(group)))
-        b.append(logisticRegression(shuffle(group)))
-        c.append(compute_svm(shuffle(group)))
-    print(np.average(a, axis=0))
-    print(np.average(b, axis=0))
-    print(np.average(c, axis=0))
-    print("---------------x-------------------x------------")
+        a.append(linearRegression(group))
+        b.append(logisticRegression(group))
+        c.append(compute_svm(group))
+        d.append(decision_tree(group))
     
+    a = np.average(a, axis=0).tolist()
+    b = np.average(b, axis=0).tolist()
+    c = np.average(c, axis=0).tolist()
+    d = np.average(d, axis=0).tolist()
+
+    a = ["Linear_"+str(key)] + a
+    b = ["Logistic_"+str(key)] + b
+    c = ["SVM_"+str(key)] + c
+    d = ["Decision Tree_"+str(key)] + d
+
+    final_list = final_list+[a]+[b]+[c]+[d]
+    
+    # print("Linear:", np.average(a, axis=0))
+    # print("Logistic:",np.average(b, axis=0))
+    # print("Support Vector Machines:",np.average(c, axis=0))
+    # print("Decision Trees:",np.average(d, axis=0))
+    # print("---------------x-------------------x------------")
+    
+columns = ["Model", "TPR", "TNR", "FPR", "FNR", "Accuracy", "AUC"]
+
+df = pd.DataFrame(data= final_list, columns=columns)
+print(df)
+##  iterations 
+## relevant columns
+## balancing techiniques
+## new methods like decision trees
